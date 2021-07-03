255c96c5dbfda9868da62134415b7183921f1018
f-yoshihara <fumiro1989@gmail.com>
2021-07-03 21:14:37 +0900

    Try NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>this page is entry point</h1>
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+  </main>
 </template>
